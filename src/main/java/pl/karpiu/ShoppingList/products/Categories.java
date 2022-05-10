ac8b6299a04d98d6e7e53a5298810c9b4581f924
Bartosz Karp <bartek.karp93@gmail.com>
2022-05-10 21:54:36 +0200

    Delete creating object Categories when addProduct methods is used. Remove problem writing wrong productName using removeProduct method

--- src/main/java/pl/karpiu/ShoppingList/products/Categories.java
+++ src/main/java/pl/karpiu/ShoppingList/products/Categories.java
@@ -5,43 +5,22 @@ import java.util.List;
 
 public class Categories {
 
-<<<<<<< HEAD
     private final List<String> shoppingList = new ArrayList<>();
 
-    public Categories(){
-=======
-    private final ArrayList<String> shoppingList = new ArrayList<String>();
-
-    public void addCategories(){
->>>>>>> origin
-        shoppingList.add(0, "[Product categories - examples]");
-        shoppingList.add(1, "Bakery");
-        shoppingList.add(2, "Meat");
-        shoppingList.add(3, "Fruit");
-        shoppingList.add(4, "Alcohol");
-        shoppingList.add(5, "Toiletries");
-        shoppingList.add(6, "Pet shop");
-        shoppingList.add(7, "Other");
-    }
-
-<<<<<<< HEAD
-      public void showCategories() {
-         for (String categoriesName : shoppingList) {
-              System.out.println(categoriesName);
-          }
-=======
-
-    public void showCategories() {
-
-
-        System.out.println("Choose product categories:");
-
-
-        for (int i = 0; i < shoppingList.size(); i++) {
-
-            System.out.println(i + ". " + shoppingList.get(i));
-
+    public Categories() {
+            shoppingList.add(0, "[Product categories - examples]");
+            shoppingList.add(1, "Bakery");
+            shoppingList.add(2, "Meat");
+            shoppingList.add(3, "Fruit");
+            shoppingList.add(4, "Alcohol");
+            shoppingList.add(5, "Toiletries");
+            shoppingList.add(6, "Pet shop");
+            shoppingList.add(7, "Other");
         }
->>>>>>> origin
+
+        public void showCategories() {
+            for (String categoriesName : shoppingList) {
+                System.out.println(categoriesName);
+            }
+        }
     }
-}
